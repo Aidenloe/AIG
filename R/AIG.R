d5fe414e6c85a7b6268a8595334b52af650272c0
Aiden Loe <bsl28@cam.ac.uk>
2016-12-13 11:41:48 +0000

    changes

---
+++ R/AIG.R
@@ -0,0 +1,11 @@
+#' AIG: A package for studying automatic item generators.
+#'
+#' The AIG package contains methods for studying automatic item generators. The package includes several item types generations for research purposes. If the generator has many related functions, then it will be pushed into a new package instead. Other generators include, netGen, mazeGen, numGen.
+#'
+#' Still need to make changes here.
+#' @section Linear Syllogism Generation:
+#' This function creates linear syllogsim tasks accordingly to your specifications. \cr
+#' \code{\link{lisy}}
+#'
+#' @name AIG
+NULL
