bf04f4045ecdb55d8f033db696831dc926738571
bsl28@cam.ac.uk <bsl28@cam.ac.uk>
2017-01-22 18:13:26 +0000

    new changes

--- R/ARMadd.R
+++ R/ARMadd.R
@@ -6,7 +6,7 @@
 #' @param numOps sets the number of operations in the stimuli (range of values is 1 to 5)
 #' @param magnitude sets the range of values for the integers in the stimuli by powers of 10 (range of values is 1 to 5)
 #' @param sameValue  Deciding if you want the values to be the same or different values.
-#' @param arithOps There are two options in this argument, either 'add' or 'subtract'.
+#' @param arithOps There are two options in this argument, either 'add' or 'subtract', "multiply.
 #' @param allowNeg Allow the use of negative values. Only for "add" arithmetic operator. Not allowed for "subtract".
 #' @param order There are three options to order the values presented. "mixed", "decreasing", "increasing". Does not matter when same Value = TRUE.
 #' @return
@@ -24,17 +24,17 @@
 #'
 #'
 
-ICARarith <- function(numProbs=4, numOps = 1, magnitude = 1, arithOps = "add", allowNeg = FALSE, sameValue=FALSE, order = "mixed"){
+ICARarith <- function(numProbs=5, numOps = 1, magnitude = 1, arithOps = "add", allowNeg = FALSE, sameValue=FALSE, order = "mixed"){
   # Addition
   # numOps sets the number of operations in the stimuli (range of values is 1 to 5)
   # magnitude sets the range of values for the integers in the stimuli by powers of 10 (range of values is 1 to 5)
 
-   # numProbs=2
-   # numOps = 2
-   # magnitude = 3
-   # allowNeg = FALSE
-   # sameValue=TRUE
-   # arithOps = "subtract"
+   # numProbs= 1000
+   # numOps = 1
+   # magnitude = 1
+   # allowNeg = TRUE
+   # sameValue=FALSE
+   # arithOps = "multiply"
    # order="mixed"
 
    possible <- c(1:5) #maximum operators allowed
@@ -60,18 +60,41 @@ ICARarith <- function(numProbs=4, numOps = 1, magnitude = 1, arithOps = "add", a
     for(Probs in 1:numProbs) { #begin a loop to generate a X number of problems
     # Select the integers to be used in the operations
 
+      zero = TRUE
       #sample numbers
     if(sameValue == FALSE){
       if(order == "ascending"){
+        while(zero==TRUE){ # make sure no zeros are used
         (stimuli <- sort(sample(number_diff,  num_levels, replace=T))) #question
+          while((length(unique(stimuli))==1)==TRUE){ # never get same values
+            (stimuli <- sample(number_diff,  num_levels, replace=T)) #question
+          }
+          if(!0 %in% stimuli){ zero <-  FALSE} # stopping rule
+        }
       }
+
       if(order == "descending"){
+        while(zero==TRUE){ # make sure no zeros are used
         (stimuli <- sort(sample(number_diff,  num_levels, replace=T), decreasing=TRUE)) #question
+          while((length(unique(stimuli))==1)==TRUE){ # never get same values
+            (stimuli <- sample(number_diff,  num_levels, replace=T)) #question
+          }
+          if(!0 %in% stimuli){zero = FALSE} # stopping rule
+        }
       }
       if(order == "mixed"){
+        {
+          while(zero==TRUE){ # make sure no zeros are used
         (stimuli <- sample(number_diff,  num_levels, replace=T)) #question
+            while((length(unique(stimuli))==1)==TRUE){ # never get same values
+              (stimuli <- sample(number_diff,  num_levels, replace=T)) #question
+            }
+          if(!0 %in% stimuli){zero = FALSE} # stopping rule
+        }
       }
     }
+  }
+
 
     # if users wants to have same numeric value
       # if(sameValue==TRUE){
@@ -90,8 +113,12 @@ ICARarith <- function(numProbs=4, numOps = 1, magnitude = 1, arithOps = "add", a
 
       # if users wants to have same numeric value
       if(sameValue==TRUE){
-        (stimuli <- sample(number_diff,  num_levels-numOps, replace=T))
-        (stimuli<- rep(stimuli, num_levels))
+        zero <- TRUE
+        while(zero==TRUE){ # make sure no zeros are used
+            (stimuli <- sample(number_diff,  num_levels-numOps, replace=T))
+            (stimuli<- rep(stimuli, num_levels))
+          if(!0 %in% stimuli){zero = FALSE} # stopping rule
+        }
       }
 
 
@@ -107,11 +134,17 @@ ICARarith <- function(numProbs=4, numOps = 1, magnitude = 1, arithOps = "add", a
     (answer <- sum(stimuli))
   }
 
+  if(arithOps == "multiply"){
+    (stimuliCode <- stimuli) #used in the response option only
+    (answer <- prod(stimuli))
+    if(numOps > 1) message("The answer is likely to be very big. Consider reducing the numOps.")
+    if(magnitude > 2) message("The answer is likely to be very big. Consider reducing the magnitude.")
+      }
+
+     answer2 <- cbind(answer, "answer")
 
     (posmin <- sample(10, 7)) #why 10 and why 7?
     (posmin <- ifelse(posmin>5, 1, -1)) # if greater than 5 all 1, if not -1. Why?
-
-#### The incorrect answers are referred to as the distractors and typically represent common errors that are equally plausible only to students who have not attained the level of knowledge or understanding necessary to recognize the key as the correct answer (Linn and Gronlund, 2000; Popham 2000; Nitko, 2004)
 
     ### 7 Distractors created###
     (near_answer1 <- round(.5+(sample(20,1)*.1)))
@@ -125,38 +158,117 @@ ICARarith <- function(numProbs=4, numOps = 1, magnitude = 1, arithOps = "add", a
 
     (near_answers <- c(near_answer1, near_answer2, near_answer3, near_answer4, near_answer5, near_answer6, near_answer7))
 
-
-    unique(near_answers) #keep only unique numbers
-    posmin[1:length(unique(near_answers))] #extract data from the position of the length of unique numbers
-
-    unique(near_answers)*posmin[1:length(unique(near_answers))] # making it positive or negative, why multiple? maybe additive might be closer to the actual answer?
-    #unique(near_answers)+posmin[1:length(unique(near_answers))]
-
     # distractors that are near the answers. Dependent on the sample above.
     (near_answers <- answer+(unique(near_answers)*posmin[1:length(unique(near_answers))]))
+    (near_answers2 <- as.matrix(near_answers))
+    (near_answers2 <- cbind(near_answers2, rep("dist1a", nrow(near_answers2)))) # near_answer matrix
 
     (opp_answer <- -1*answer) # negative answer as a problem of misunderstanding operator rules.
+    opp_answer2 <- as.matrix(opp_answer)
+    (opp_answer2 <- cbind(opp_answer,(rep("oppsite",nrow(opp_answer2)))))
 
-    # Get a reasonable range of choices for the remaining distractors ####
+    # Get a reasonable range of choices for the remaining distractors (DISTRACTORS 2) ####
+    if(arithOps=="add" || arithOps=="subtract"){
     (range_max <- num_levels*(max+1)) #upper bound
     ifelse (allowNeg == TRUE, range_min <- -(num_levels*(max+1)), range_min <- 0) #lower bound
     (range_choices <- setdiff(range_min:range_max, answer)) #all choices except answer
     (range_choices <- setdiff(range_choices, near_answers)) #all choices except answer and near answers
     (range_choices <- if(allowNeg) setdiff(range_choices, opp_answer) else c(range_choices)) # only if allowNeg is possible
 
-
     # Select the distractors to insert in the response choices ####
     (distractors1 <- sample(range_choices, 4, replace=F)) #only 4 distractors of leftover.
     (distractors2 <- if(allowNeg) c(distractors1, near_answers, opp_answer) else c(distractors1, near_answers))
+    (distractors2 <- as.matrix(distractors1))
+    (distractors3 <- cbind(distractors2, rep("dist1b",nrow(distractors2))))
+    (distractors4 <- rbind(distractors3, near_answers2, opp_answer2)) # combine all together.
 
-    (distractors2 <- unique(distractors2))
-    (distractors2 <- sample(distractors2, 6, replace=F))
+    distractors4 <- distractors4[!duplicated(distractors4[,1]),]  # remove duplicated distractors
+    (distractors4 <- distractors4[sample(nrow(distractors4), 6,replace=FALSE), ]) # draw 6 randomly.
+    }
 
+    if(arithOps=="multiply"){
+    dist<- 6 # need to have at least 6 distractors
+      while(i < 6){
+      if(sameValue==FALSE){
+          # Distractor Type 2
+          diffDistractor <- FALSE
+          while(diffDistractor == FALSE){
+              range_choices2 <- NULL
+              for(k in 1:5){
+                (range_choices <- stimuli )
+                (dist <- sample(length(stimuli),1)) #randomly draw one value from the stimuli
+                (range_choices[dist]     <-   stimuli[dist]+ sample(1:3,1)) # randomly adding either 1,2,3 to a single value
+                (range_choices2[k] <-prod(range_choices)) #create 5 distractors
+
+              }
+              if(anyDuplicated(range_choices2)==0){ #kill only when all distractors are different values
+                diffDistractor <- TRUE
+                  }
+            }
+
+
+      # Select the distractors to insert in the response choices ####
+      (distractors1 <- sample(range_choices2, 5, replace=F)) #only 4 distractors of leftover.
+      if(allowNeg) {
+        (negValue   <- sample(length(distractors1),2)) #select two values
+        #(distractors1[negValue] <- abs(distractors1[negValue]))
+        (distractors1 <- c(distractors1, abs(distractors1[negValue]))) # flip the 2 values to abs.
+        (distractors2 <- as.matrix(distractors1))
+        (distractors3 <- cbind(distractors2, rep("dist2a",nrow(distractors2))))
+        (distractors4 <- rbind(distractors3, near_answers2, opp_answer2)) # combine all together.
+      }else{
+        distractors2 <- as.matrix(distractors1)
+        (distractors3 <- cbind(distractors2, rep("dist2a",nrow(distractors2))))
+        (distractors4  <-rbind(distractors3, near_answers2))
+        }
+      }
+
+      if(sameValue==TRUE){
+        # Distractor Type 2
+        diffDistractor <- FALSE
+        while(diffDistractor == FALSE){
+          range_choices2 <- NULL
+          for(k in 1:5){
+            (range_choices <- stimuli)
+            (dist <- sample(length(stimuli),1)) #randomly draw one value from the stimuli
+            (range_choices[dist]     <-   stimuli[dist] + sample(1:3,1)) # randomly adding either 1,2,3 to a single value
+            (dist <- sample(length(range_choices),1)) #randomly draw a value from the range_choices now
+            (range_choices[dist]     <-   range_choices[dist] + sample(1:3,1)) # randomly adding either 1,2,3 to a single value
+            (dist <- sample(length(range_choices),1)) #randomly draw another value from the range_choices now
+            (range_choices[dist]     <-   range_choices[dist] + sample(1:3,1)) # randomly adding either 1,2,3 to a single value
+            (range_choices2[k] <-prod(range_choices)) #create 5 distractors
+          }
+          range_choices2
+          if(anyDuplicated(range_choices2)==0){ #kill only when all distractors are different values
+            diffDistractor <- TRUE
+          }
+        }
+        # Select the distractors to insert in the response choices ####
+        (distractors1 <- sample(range_choices2, 5, replace=F)) #only 4 distractors of leftover.
+        if(allowNeg) {
+          (negValue   <- sample(length(distractors1),2)) #select two values
+          #(distractors1[negValue] <- abs(distractors1[negValue])) # flip the 2 values to abs.
+          distractors1 <- c(distractors1, abs(distractors1[negValue])) # flip the 2 values to abs.
+          (distractors2 <- as.matrix(distractors1))
+          (distractors3 <- cbind(distractors2, rep("dist2b",nrow(distractors2))))
+          (distractors4 <- rbind(distractors3, near_answers2, opp_answer2)) # combine all together.
+        }else{
+          distractors2 <- as.matrix(distractors1)
+          (distractors3 <- cbind(distractors2, rep("dist2b",nrow(distractors2))))
+          (distractors4  <-rbind(distractors3, near_answers2))
+        }
+      }
+
+
+        (distractors4 <-distractors4[!duplicated(distractors4[,1]),])  # remove duplicated distractors
+        if(nrow(distractors4)>5){i <- 6}
+      }
+      (distractors4 <- distractors4[sample(nrow(distractors4), 6,replace=FALSE), ]) # draw 6 randomly.
+    }
 
     # Make the list of response choices
-    (responses <- c(distractors2, answer))
-    (responses <- sample(responses, 7, replace=F))
-
+    (responses <- rbind(distractors4, answer2))
+    (responses <- responses[sample(nrow(responses), 7,replace=FALSE), ]) # draw 6 randomly.
 
     #### Write the item prompt text ####
 if(arithOps == "add"){
@@ -182,12 +294,11 @@ if(arithOps == "subtract"){
   if (numOps == 1) {
     prompt <- paste("Choose the best answer: ", abs(stimuli[1]), " - ", abs(stimuli[2]), " = ?", sep="")
   }
-
   if (numOps == 2) {
     prompt <- paste("Choose the best answer: ", abs(stimuli[1]), " - " , abs(stimuli[2]),  " - ", abs(stimuli[3]), " = ?", sep="")
   }
   if (numOps == 3) {
-    prompt <- paste("Choose the best answer: ", abs(stimuli[1]), abs(stimuli[2]) , " - ", abs(stimuli[3]), " - " , abs(stimuli[4]), " = ?", sep="")
+    prompt <- paste("Choose the best answer: ", abs(stimuli[1]),  " - " ,abs(stimuli[2]) , " - ", abs(stimuli[3]), " - " , abs(stimuli[4]), " = ?", sep="")
   }
   if (numOps == 4) {
     prompt <- paste("Choose the best answer: ", abs(stimuli[1]),  " - ", abs(stimuli[2]), " - ", abs(stimuli[3]), " - ", abs(stimuli[4]), " - ", abs(stimuli[5]), " = ?", sep="")
@@ -196,37 +307,78 @@ if(arithOps == "subtract"){
     prompt <- paste("Choose the best answer: ", abs(stimuli[1]), " - ", abs(stimuli[2]), " - ", abs(stimuli[3]), " - ", abs(stimuli[4]), " - ", abs(stimuli[5]), " - ", abs(stimuli[6]), " = ?", sep="")
   }
 }
+
+
+
+    if(arithOps == "multiply"){
+      if (numOps == 1) {
+        prompt <- paste("Choose the best answer: ", stimuli[1], " x ", stimuli[2], " = ?", sep="")
+      }
+      if (numOps == 2) {
+        prompt <- paste("Choose the best answer: ", stimuli[1], " x " , stimuli[2],  " x ", stimuli[3], " = ?", sep="")
+      }
+      if (numOps == 3) {
+        prompt <- paste("Choose the best answer: ", stimuli[1], " x ",stimuli[2], " x ", stimuli[3], " x " , stimuli[4], " = ?", sep="")
+      }
+      if (numOps == 4) {
+        prompt <- paste("Choose the best answer: ", stimuli[1],  " x ", stimuli[2], " x ", stimuli[3], " x ", stimuli[4], " x ", stimuli[5], " = ?", sep="")
+      }
+      if (numOps == 5) {
+        prompt <- paste("Choose the best answer: ", stimuli[1], " x ", stimuli[2], " x ", stimuli[3], " x ", stimuli[4], " x ", stimuli[5], " x ", stimuli[6], " = ?", sep="")
+      }
+    }
+
 prompt
 answer
     ### Response option ####
 if(arithOps == "add") (code <- paste(ifelse(stimuli < 0, 1, 0), sep="", collapse=""))
 if(arithOps == "subtract") (code <- paste(ifelse(stimuliCode < 0, 1, 0), sep="", collapse=""))
-
+if(arithOps == "multiply") (code <- paste(ifelse(stimuli < 0, 1, 0), sep="", collapse=""))
     (code <- paste(magnitude, "_", code, sep=""))
-    # Write the item response choice text
-    (choices <- paste("(1), ", responses[1], " ,(2), ", responses[2], ", (3), ", responses[3], ", (4), ", responses[4], " ,(5), ", responses[5], " ,(6), ", responses[6], " ,(7), None of these ,(8), I don't know", sep=""))
 
+#    (choices <- paste("(1), ", responses[1,], " ,(2), ", responses[2,], ", (3), ", responses[3,], ", (4), ", responses[4,], " ,(5), ", responses[5,], " ,(6), ", responses[6,], " ,(7), None of these ,(8), I don't know", sep=""))
+# (choices <- data.frame(response1 =responses[1,],
+#                       response2 =  responses[2,],
+#                       response3 = responses[3,],
+#                       response4 = responses[4,],
+#                       response5=  responses[5,],
+#                       response6=  responses[6,],
+#                       response7= paste("None of these"),
+#                       response8= paste("I don't know")))
+
+    # Write the item response choice text
+    (choices<- c(responses[1,],responses[2,],responses[3,],responses[4,],responses[5,],responses[6,],"None of these", "I don't know"))
 
     # Write the correct answer text
-    (correct <- paste("(", match(answer, responses), ") ", answer, sep=""))
+    # (correct <- paste("(", match(answer, responses), ") ", answer, sep=""))
+    (correct <- answer)
 
     ##### Put it all together ####
-    results[[Probs]] <- list(stimuli = prompt,
-                             choices = choices,
-                             answer = correct,
-                             code = code)
+        # item <-   data.frame(stimuli = prompt,
+        #                      choices = choices,
+        #                      answer = correct,
+        #                      code = code)
+      item <-  c(prompt,code,correct,choices)
+      (item <- t(as.data.frame(item)))
+        colnames(item) <- c("prompt","code","answer", "option 1","distType1","option 2","distType2","option 3","distType3","option 4","distType4","option 5","distType5","option 6","distType6","option 7","option 8")
+        (results[[Probs]] <- item)
 
   }
 
+
   # Now, do some reorganization so the problems are easier to handle
-  results <- unlist(results)
-  results <- matrix(results,ncol=numProbs)
-  (results <- t(results))
-  colnames(results) <- c("prompt", "choices", "answer", "code")
-  results <- as.data.frame(results)
-  return(results)
+  (results1 <- do.call(rbind,results))
+  rownames(results1) <- 1:nrow(results1)
+  #(results2 <- as.data.frame(results1))
+
+  # results <- matrix(results,ncol=numProbs)
+  # (results <- t(results))
+  # colnames(results) <- c("prompt", "choices", "answer", "code")
+  class(results1) <- "ICARarith"
+  return(results1)
 }
 
 
-#ICARarith(numProbs=4, numOps = 4, magnitude = 3, arithOps = "subtract",allowNeg = FALSE, sameValue=FALSE, order = "mixed")
+#a <- ICARarith(numProbs=10000, numOps = 1, magnitude = 2, arithOps = "multiply",allowNeg = FALSE, sameValue=FALSE, order = "descending")
+
 
--- R/classes.R
+++ R/classes.R
@@ -16,18 +16,29 @@ print.lisy <- function(x,...){
 }
 
 
+# #' @export
+# #'
+# print.ICARarith <- function(x,...){
+#   results<- x
+#   cat("Arithmetic Questions:")
+#   cat(paste0("\n",results$prompt))
+#   cat("\n \nResponse Options:")
+#   cat(paste0("\n (1) ",results$`option 1`," [",results$distType1 ,"] "  ,"(2) ",results$`option 2`," [",results$distType2 ,"] " , "(3) ",results$`option 3`," [",results$distType3 ,"] " , "(4) ",results$`option 4`," [",results$distType4 ,"] " , "(5) ",results$`option 5`," [",results$distType5 ,"] " , "(6) ",results$`option 6`," [",results$distType6 ,"] " , "(7) ",results$`option 7`, " (8) ",results$`option 8`))
+#   cat("\n\nAnswers:")
+#   cat(paste0("\n",results$answer))
+#   cat("\n\nCode:")
+#   cat(paste0("\n",results$code))
+# }
+
+
 #' @export
 #'
 print.ICARarith <- function(x,...){
   results<- x
   cat("Arithmetic Questions:")
-  cat(paste0("\n",results$prompt))
-  cat("\n\nResponse Options:")
-  cat(paste0("\n",results$choices))
-  cat("\n\nAnswers:")
-  cat(paste0("\n",results$answer))
-  cat("\n\nCode:")
-  cat(paste0("\n",results$code))
+  cat(paste0("\n",results[,1]))
+  cat("\n \nResponse Options:\n")
+  print(as.data.frame(results[,2:17]))
 }
 
 
